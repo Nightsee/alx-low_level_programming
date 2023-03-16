1eda0520f46d6f23918ec9f86936a63af23091f5
Nightsee <younes.naitsi.2002@gmail.com>
2023-03-16 08:38:36 -0700

    first half done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * description: printing a quote using write function
+ * write(int fd. const void *buf. size_t count)
+ *
+ * Return: Always 1 (Success)
+*/
+
+
+int main(void)
+{
+	char quoteToPrint[] = "and that piece of art is useful\" - Dora Korgar, 2015-10-10\n";
+
+	write(2, quoteToPrint, 59);
+	return (1);
+}
