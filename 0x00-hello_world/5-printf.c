a52203d7bc680913a15e28676c2b8e847a2984d0
Nightsee <younes.naitsi.2002@gmail.com>
2023-03-16 07:43:32 -0700

    first half done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+/*
+ * the main function takes nothing as argument, and it returns 0
+ *
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art, ");
