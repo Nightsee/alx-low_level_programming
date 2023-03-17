6c016af510a2cd7a54a80c0cbc24c4de7c11c74e
Nightsee <younes.naitsi.2002@gmail.com>
2023-03-17 08:36:42 -0700

    all mandatory tasks done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 5; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
-	for (i = 61; i < 67; i++)
+	for (i = 97; i < 103; i++)
 	{
 		putchar(i);
 	}
