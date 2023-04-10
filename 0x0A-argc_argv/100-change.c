7110d8f6fd20a03b6d8a055f215f9b22841d9ccf
Nightsee <younes.naitsi.2002@gmail.com>
2023-04-10 08:56:35 -0700

    advanced 0x0A task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the min number of coins to make change for an amount
+ * @argc: number of arguments
+ * @argv: array containing the arguments
+ * Return: 0 for success and 1 for error
+*/
+
+
+int main(int argc, char *argv[])
+{
+	int cents, coins;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	if (cents < 0)
+		printf("0");
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		coins += 1;
+	}
+	printf("%d\n", coins);
+
+	return (0);
+}
