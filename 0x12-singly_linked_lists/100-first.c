323a42982ac26880aa1f41f8a7e1c3f5acc429e9
Nightsee <younes.naitsi.2002@gmail.com>
2023-04-28 01:47:06 -0700

    fixing 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
+/**
+ * before_main - this function is executed before the main function
+ * Return: nothing
+*/
 void __attribute__((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
