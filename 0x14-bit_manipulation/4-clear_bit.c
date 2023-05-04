26af1d57abd1e4c028638ad3f5507df8275580f0
Nightsee <younes.naitsi.2002@gmail.com>
2023-05-04 13:13:45 -0700

    bit 4 fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,7 +8,7 @@
  * Return: 1 if it worked, -1 if it didn't.
 */
 
-int clear_bit(unsigned ling int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i;
 	unsigned int hold;
